e9a374882bc72369f522efc6b445ee80c7f229ab
Kaartik Arora <kaartikarora0001@gmail.com>
2026-03-25 01:22:07 +0530

    Implemented the Performance Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
 // ScrollTrigger -> let us animate things based on the scroll position 
 import Showcase from './components/Showcase'
+import Performance from './components/Performance'
+import Features from './components/Features'
+import Highlights from './components/Highlights'
+import Footer from './components/Footer'
 
 gsap.registerPlugin(ScrollTrigger)
 // this line will makes sure your gsap plugin is 
@@ -17,6 +21,10 @@ const App = () => {
       <Hero />
       <ProductViewer />
       <Showcase />
+      <Performance />
+      <Features />
+      <Highlights />
+      <Footer />
     </main>
   )
 }
